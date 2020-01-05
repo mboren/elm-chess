eab8a9a63b8ee1d3013da055294467f2a3703400
Marty Boren <martyboren@gmail.com>
2020-01-05 12:41:31 -0800

    Add test for parsing castling

--- tests/TestPgnParsing.elm
+++ tests/TestPgnParsing.elm
@@ -1,10 +1,12 @@
 module TestPgnParsing exposing (..)
 
+import Array exposing (Array)
+import Array2D exposing (Array2D)
 import Expect exposing (Expectation)
 import Piece exposing (Piece, PieceKind(..))
 import Player exposing (Player(..))
 import Ply exposing (Ply(..))
-import Position
+import Position exposing (Position)
 import Square exposing (Square)
 import Test exposing (..)
 
@@ -12,6 +14,11 @@ import Test exposing (..)
 basicPly : Square -> Square -> Piece.PieceKind -> Player -> Ply
 basicPly start end pieceKind player =
     Ply.StandardMove { start = start, end = end, piece = Piece pieceKind player, player = player, takes = Nothing, promotion = Nothing }
+
+
+ruyLopezWithWhiteKingsideCastlePosition : Position
+ruyLopezWithWhiteKingsideCastlePosition =
+    { board = { columns = 8, data = Array.fromList [ Array.fromList [ Just { color = WhitePlayer, kind = Rook }, Nothing, Just { color = WhitePlayer, kind = Bishop }, Just { color = WhitePlayer, kind = Queen }, Nothing, Just { color = WhitePlayer, kind = Rook }, Just { color = WhitePlayer, kind = King }, Nothing ], Array.fromList [ Just { color = WhitePlayer, kind = Pawn }, Just { color = WhitePlayer, kind = Pawn }, Just { color = WhitePlayer, kind = Pawn }, Just { color = WhitePlayer, kind = Pawn }, Nothing, Just { color = WhitePlayer, kind = Pawn }, Just { color = WhitePlayer, kind = Pawn }, Just { color = WhitePlayer, kind = Pawn } ], Array.fromList [ Nothing, Just { color = WhitePlayer, kind = Bishop }, Just { color = WhitePlayer, kind = Knight }, Nothing, Nothing, Just { color = WhitePlayer, kind = Knight }, Nothing, Nothing ], Array.fromList [ Nothing, Nothing, Nothing, Nothing, Just { color = WhitePlayer, kind = Pawn }, Nothing, Nothing, Nothing ], Array.fromList [ Nothing, Just { color = BlackPlayer, kind = Pawn }, Nothing, Just { color = BlackPlayer, kind = Pawn }, Just { color = BlackPlayer, kind = Pawn }, Nothing, Nothing, Nothing ], Array.fromList [ Just { color = BlackPlayer, kind = Pawn }, Nothing, Just { color = BlackPlayer, kind = Knight }, Nothing, Nothing, Just { color = BlackPlayer, kind = Knight }, Nothing, Nothing ], Array.fromList [ Nothing, Nothing, Just { color = BlackPlayer, kind = Pawn }, Nothing, Nothing, Just { color = BlackPlayer, kind = Pawn }, Just { color = BlackPlayer, kind = Pawn }, Just { color = BlackPlayer, kind = Pawn } ], Array.fromList [ Just { color = BlackPlayer, kind = Rook }, Nothing, Just { color = BlackPlayer, kind = Bishop }, Just { color = BlackPlayer, kind = Queen }, Just { color = BlackPlayer, kind = King }, Just { color = BlackPlayer, kind = Bishop }, Nothing, Just { color = BlackPlayer, kind = Rook } ] ] }, history = { latestPly = Just (KingsideCastle WhitePlayer), pastMoves = [ ( StandardMove { end = { file = 2, rank = 2 }, piece = { color = WhitePlayer, kind = Knight }, player = WhitePlayer, promotion = Nothing, start = { file = 1, rank = 0 }, takes = Nothing }, StandardMove { end = { file = 3, rank = 4 }, piece = { color = BlackPlayer, kind = Pawn }, player = BlackPlayer, promotion = Nothing, start = { file = 3, rank = 6 }, takes = Nothing } ), ( StandardMove { end = { file = 1, rank = 2 }, piece = { color = WhitePlayer, kind = Bishop }, player = WhitePlayer, promotion = Nothing, start = { file = 0, rank = 3 }, takes = Nothing }, StandardMove { end = { file = 5, rank = 5 }, piece = { color = BlackPlayer, kind = Knight }, player = BlackPlayer, promotion = Nothing, start = { file = 6, rank = 7 }, takes = Nothing } ), ( StandardMove { end = { file = 0, rank = 3 }, piece = { color = WhitePlayer, kind = Bishop }, player = WhitePlayer, promotion = Nothing, start = { file = 1, rank = 4 }, takes = Nothing }, StandardMove { end = { file = 1, rank = 4 }, piece = { color = BlackPlayer, kind = Pawn }, player = BlackPlayer, promotion = Nothing, start = { file = 1, rank = 6 }, takes = Nothing } ), ( StandardMove { end = { file = 1, rank = 4 }, piece = { color = WhitePlayer, kind = Bishop }, player = WhitePlayer, promotion = Nothing, start = { file = 5, rank = 0 }, takes = Nothing }, StandardMove { end = { file = 0, rank = 5 }, piece = { color = BlackPlayer, kind = Pawn }, player = BlackPlayer, promotion = Nothing, start = { file = 0, rank = 6 }, takes = Nothing } ), ( StandardMove { end = { file = 5, rank = 2 }, piece = { color = WhitePlayer, kind = Knight }, player = WhitePlayer, promotion = Nothing, start = { file = 6, rank = 0 }, takes = Nothing }, StandardMove { end = { file = 2, rank = 5 }, piece = { color = BlackPlayer, kind = Knight }, player = BlackPlayer, promotion = Nothing, start = { file = 1, rank = 7 }, takes = Nothing } ), ( StandardMove { end = { file = 4, rank = 3 }, piece = { color = WhitePlayer, kind = Pawn }, player = WhitePlayer, promotion = Nothing, start = { file = 4, rank = 1 }, takes = Nothing }, StandardMove { end = { file = 4, rank = 4 }, piece = { color = BlackPlayer, kind = Pawn }, player = BlackPlayer, promotion = Nothing, start = { file = 4, rank = 6 }, takes = Nothing } ) ] }, playerToMove = BlackPlayer }
 
 
 suite : Test
@@ -76,5 +83,8 @@ suite =
             , test "Ambiguous knight move with start pos should succeed" <|
                 \_ ->
                     Expect.ok (Position.fromPgn "1. Nf3 a6 2. Nc3 a5 3. Nd4 a4 4. Nc3b5")
+            , test "Ruy lopez with white kingside castle" <|
+                \_ ->
+                    Expect.equal (Position.fromPgn "1. e4 e5 2. Ng1f3 Nb8c6 3. Bb5 a6 4. Ba4 b5 5. Bb3 Ng8f6 6. Nb1c3 d5 7. O-O") (Ok ruyLopezWithWhiteKingsideCastlePosition)
             ]
         ]
